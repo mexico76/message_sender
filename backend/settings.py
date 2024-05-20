621ced75e874826e11281763bb9d12902a227fb8
Mikhail Makarov <35921070+mexico76@users.noreply.github.com>
2024-05-20 20:00:54 +0300

    Update settings.py empty token

--- backend/settings.py
+++ backend/settings.py
@@ -3,7 +3,7 @@ DB_USER = "postgres"
 DB_PASS = "password"
 DB_NAME = "sender_db"
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODAxNzYyNDIsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6Ik1pa2hhaWwifQ.imk76XDQq7vyomWF5hYVYjgbu7mUjOlJekfmmsPZO58"
+token = ""
 
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_RESULT_BACKEND = "redis://redis:6379/0"
